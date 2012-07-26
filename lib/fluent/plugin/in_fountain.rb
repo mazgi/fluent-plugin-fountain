c4c2f3bc583475fed37b99d5b11fdcaac61fa253
MATSUKI.Hidenori <MATSUKI.Hidenori@gmail.com>
2012-07-26 21:05:22 +0900

    create base

--- lib/fluent/plugin/in_fountain.rb
+++ lib/fluent/plugin/in_fountain.rb
@@ -1,9 +1,35 @@
-require "fluent-plugin-fountain/version"
-
 module Fluent
-  module Plugin
-    module Fountain
-      # Your code goes here...
+class FountainInput < Input
+  Plugin.register_input('fountain', self)
+
+  def initialize
+    super
+    @paths = []
+  end
+
+  config_param :path, :string
+
+  attr_reader :paths
+
+  def configure(conf)
+    super
+
+    @paths = @path.split(',').map {|path| path.strip }
+    if @paths.empty?
+      raise ConfigError, "fountain: 'path' parameter is required on fountain input"
     end
   end
+
+  def start
+    super
+  end
+
+  def shutdown
+    super
+  end
+
+  def run
+    super
+  end
+end
 end
