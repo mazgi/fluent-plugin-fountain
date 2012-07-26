7a704250c945bf194456aaab0cdd1f60dfcb441f
MATSUKI.Hidenori <MATSUKI.Hidenori@gmail.com>
2012-07-26 21:09:20 +0900

    create test base

---
+++ test/plugin/test_in_fountain.rb
@@ -0,0 +1,27 @@
+require 'fluent/test'
+
+class FountainInputTest < Test::Unit::TestCase
+  def setup
+    Fluent::Test.setup
+    FileUtils.rm_rf(TMP_DIR)
+    FileUtils.mkdir_p(TMP_DIR)
+  end
+
+  TMP_DIR = File.dirname(__FILE__) + "/../tmp"
+
+  CONFIG = %[
+    path #{TMP_DIR}/**/*
+  ]
+
+  def create_driver(conf=CONFIG)
+    Fluent::Test::InputTestDriver.new(Fluent::FountainInput).configure(conf)
+  end
+
+  def test_configure
+    d = create_driver
+    assert_equal ["#{TMP_DIR}/**/*"], d.instance.paths
+  end
+
+  def test_emit
+  end
+end
